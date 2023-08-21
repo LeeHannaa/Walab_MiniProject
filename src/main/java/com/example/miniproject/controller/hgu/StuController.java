e5bf12428d181a54e7c6b411c67ae0ca283674b1
LeeHannaa <tiger5391@naver.com>
2023-08-21 23:15:07 +0900

    getStuInfoById

--- src/main/java/com/example/miniproject/controller/hgu/StuController.java
+++ src/main/java/com/example/miniproject/controller/hgu/StuController.java
@@ -2,10 +2,7 @@ package com.example.miniproject.controller.hgu;
 
 import com.example.miniproject.entity.StuInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +16,10 @@ public class StuController {
     @GetMapping(value = "/stuInfo")
     public List<StuInfo> getAllStuInfo() {
         return studentService.getAllStuInfo();
+    }
+
+    @GetMapping(value = "/stuInfo/{studentId}")
+    public StuInfo getStuInfoById(@PathVariable Long studentId) {
+        return studentService.getStuInfoById(studentId);
     }
 }
--- src/main/java/com/example/miniproject/controller/hgu/StudentService.java
+++ src/main/java/com/example/miniproject/controller/hgu/StudentService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -15,5 +16,11 @@ public class StudentService {
 
     public List<StuInfo> getAllStuInfo() {
         return stuInfoRepository.findAll();
+    }
+
+    public StuInfo getStuInfoById(Long studentId) {
+        // 학생 ID를 사용하여 해당 학생의 정보 조회
+        Optional<StuInfo> student = stuInfoRepository.findById(studentId);
+        return student.orElse(null); // 해당 학생이 없으면 null 반환 또는 예외 처리
     }
 }
