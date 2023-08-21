fc07bc9be8e190954ae16fff2352661cc6597599
LeeHannaa <tiger5391@naver.com>
2023-08-21 14:35:44 +0900

    front connect

--- src/main/java/com/example/miniproject/controller/hgu/StuController.java
+++ src/main/java/com/example/miniproject/controller/hgu/StuController.java
@@ -2,6 +2,7 @@ package com.example.miniproject.controller.hgu;
 
 import com.example.miniproject.entity.StuInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/hgu")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StuController {
     private final StudentService studentService;
 
