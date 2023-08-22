dd17d25eb0ffe1c1e8941f0fb40c8a9d2cf5cf36
LeeHannaa <tiger5391@naver.com>
2023-08-22 14:56:40 +0900

    Korean resolution

--- frontend/src/components/StuInfoComponent.js
+++ frontend/src/components/StuInfoComponent.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
 import {Document, Text, Page, Font} from '@react-pdf/renderer';
-// 스타일 객체 직접 생성
+
 const styles = {
     page: {
         flexDirection: "column",
@@ -14,14 +13,21 @@ const styles = {
         fontSize: 14,
         marginBottom: 10, // 텍스트 아래 여백
         color: "#000",
+        fontFamily: 'SpoqaHanSans',
+    },
+    frame: {
+        width: "100%",
+        height: "auto",
     },
 };
 Font.register({
-    family: 'Roboto',
-    src: 'https://fonts.googleapis.com/css?family=Roboto',
+    family: 'SpoqaHanSans',
+    src:
+        'https://cdn.jsdelivr.net/gh/spoqa/spoqa-han-sans@01ff0283e4f36e159ffbf744b36e16ef742da6d8/Subset/SpoqaHanSans/SpoqaHanSansLight.ttf',
 });
-function StuInfoComponent() {
-    const { studentId } = useParams();
+
+function StuInfoComponent({ studentId }) {
+    console.log(studentId);
     const [studentInfo, setStudentInfo] = useState({});
 
     useEffect(() => {
@@ -34,16 +40,15 @@ function StuInfoComponent() {
                 console.error("학생 정보를 불러오는 중 오류 발생: ", error);
             });
     }, [studentId]);
-
     return (
         <Document>
             <Page size="A4" style={styles.page}>
-                <Text style={styles.text}>djsfjo: {studentInfo.id}</Text>
-                <Text style={styles.text}>{studentInfo.name}</Text>
-                <Text style={styles.text}>{studentInfo.department}</Text>
-                <Text style={styles.text}>{studentInfo.campname}</Text>
-                <Text style={styles.text}>{studentInfo.campstartdate}</Text>
-                <Text style={styles.text}>{studentInfo.campfinishdate}</Text>
+                <Text style={styles.text}>학번: {studentInfo.id}</Text>
+                <Text style={styles.text}>이름: {studentInfo.name}</Text>
+                <Text style={styles.text}>학부: {studentInfo.department}</Text>
+                <Text style={styles.text}>캠프 이름: {studentInfo.campname}</Text>
+                <Text style={styles.text}>캠프 시작일: {studentInfo.campstartdate}</Text>
+                <Text style={styles.text}>캠프 종료일: {studentInfo.campfinishdate}</Text>
             </Page>
         </Document>
     );
--- frontend/src/pages/PdfPage.js
+++ frontend/src/pages/PdfPage.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { PDFViewer } from '@react-pdf/renderer';
 import StuInfoComponent from "../components/StuInfoComponent";
+import {useParams} from "react-router-dom";
 
 function PdfPage() {
-
+    const { studentId } = useParams();
     return (
         <PDFViewer width="100%" height="800px">
-            <StuInfoComponent />
+            <StuInfoComponent studentId={studentId}/>
         </PDFViewer>
     );
 }
